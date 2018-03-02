bf51b040e4002bf2e836d0eaf4815aaf153bd2de
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-03-02 17:00:01 -0300

    add setup.py comments

--- setup.py
+++ setup.py
@@ -8,13 +8,36 @@ setup(name='cloneme',
       author='Luciano Serruya Aloisi',
       author_email='lucianoserruya@gmail.com',
       license='MIT',
+      #
+      # Con la funcion 'find_packages', indicamos que queremos
+      # incluir todos los paquetes que encuentre,
+      # exluyendo los que indicamos con el parámetro 'exclude'
+      #
       packages=find_packages(exclude=['*tests*']),
+      #
+      # Indicamos la versión mínima necesaria para correr
+      # nuestro paquete
+      #
       python_requires='>=3',
+      #
+      # Indicamos que dependencias
+      # (que se puedan encontrar en pip)
+      # tiene nuestro paquete
       # install_requires=[
           # 'markdown',
       # ],
+
+      #
+      # Indicamos que nuestro paquete va a generar un script
+      # entonces se lo va a poder ejecutar desde la consola
+      # ingresando 'cloneme'
+      #
       entry_points={
           'console_scripts': ['cloneme=cloneme.cloneme:main'],
       },
+      #
+      # Indicamos que queremos incluir archivos de datos
+      # como por ejemplo el README.md en nuestra distribución
+      #
       include_package_data=True,
   )
