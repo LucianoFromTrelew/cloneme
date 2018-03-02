328db164d742c24577667e6f647577881a8fbd97
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-03-02 15:52:04 -0300

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cloneme',
-      version='0.1',
+      version='0.1.1',
       description='A git clone wrapper to make cloning repos easier',
       keywords='git clone wrapper',
       url='http://github.com/LucianoFromTrelew/cloneme',
